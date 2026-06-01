5e58239b1191d7aa751982b0ae8d024987a3b520
Larissa de Andrade <las@icomp.ufam.edu.br>
2026-06-01 15:16:16 -0400

    Endurece versao segura apos analise ZAP

--- public/profile.js
+++ public/profile.js
@@ -3,3 +3,4 @@ const visitorNameElement = document.getElementById("visitor-name");
 const hashValue = decodeURIComponent(location.hash.slice(1) || "Visitante");
 
 visitorNameElement.textContent = hashValue;
+
